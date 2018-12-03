63b240f2b2fb181e9ac645fd28189995920aca1c
Jozef <jozefw@sbcglobal.net>
2018-12-02 18:27:51 -0800

    completed sign in with cookie

--- Simple Login Using Cookies/script.js
+++ Simple Login Using Cookies/script.js
@@ -4,6 +4,33 @@
 
 window.onload = function() {
 
+    if(getCookieValue('logedIn')){
+     
+        document.getElementById('login').style.display="none";
+        document.getElementById('logout').style.display="inline-block";
+        document.getElementById('user').innerHTML = getCookieValue('LogInUsername');
 
+    }else{
+        console.log('Hello Guest');
+        document.getElementById('login').style.display="inline-bock";
+        document.getElementById('logout').style.display="none";
+        document.getElementById('user').innerHTML = "Guest !";
+    }
+
+    function getCookieValue(cookieName){
+        var cookiesList = document.cookie.split(';').map(function(cooky){
+            return cooky.trim();
+        });
+    
+        for(var i=0; i < cookiesList.length; i++){
+            var cookie = cookiesList[i].split('=');
+            var cookieKey = cookie[0];
+            var cookieValue = cookie[1];
+            if(cookieKey === cookieName){
+                return cookieValue;
+            }
+        };
+        return undefined;
+    }
 
 };
